160610edff4ce160356f79e11f4bc4dede2c68e3
duckietm <troop@mailtjes.com>
2025-09-25 11:57:09 +0200

    🆕 HK OpenPositions

--- app/Models/Community/Staff/WebsiteOpenPosition.php
+++ app/Models/Community/Staff/WebsiteOpenPosition.php
@@ -1,18 +1,48 @@
 <?php
-
 namespace App\Models\Community\Staff;
 
 use App\Models\Game\Permission;
+use App\Models\Community\Staff\WebsiteStaffApplications;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
 
 class WebsiteOpenPosition extends Model
 {
     protected $guarded = ['id'];
+    protected $table = 'website_open_positions';
+    
+    use HasFactory;
 
-    public function permission(): BelongsTo
+    protected $fillable = [
+        'permission_id',
+        'description',
+        'apply_from',
+        'apply_to',
+    ];
+
+    protected $casts = [
+        'apply_from' => 'datetime',
+        'apply_to' => 'datetime',
+    ];
+
+    protected static function boot()
     {
-        return $this->belongsTo(Permission::class, 'permission_id');
+        parent::boot();
+        static::deleting(function ($openPosition) {
+            WebsiteStaffApplications::where('rank_id', $openPosition->permission_id)->delete();
+        });
+    }
+
+    public function permission()
+    {
+        return $this->belongsTo(Permission::class, 'permission_id', 'id');
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(WebsiteStaffApplications::class, 'rank_id', 'permission_id');
     }
 
     public function scopeCanApply($query)
